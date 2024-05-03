1a8d6de6b81fa14b9258d171d7c0935abb457444
faiqhumayun <faiq.hamayun@devsinc.com>
2024-05-03 15:33:19 +0500

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+mongoose
+  .connect(process.env.mongoDbUrl)
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello MERN!')
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
